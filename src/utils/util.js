dbd19730947589c0c0c5f3d3a822a023289a956d
hxiugangxh <623145066@qq.com>
2017-09-09 01:29:46 +0800

    commit

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,13 +1,12 @@
 import wepy from 'wepy'
 
 function convertToStarsArray(stars) {
-  var num = stars.toString().substring(0, 1)
-  var array = []
-  for (var i = 1; i <= 5; i++) {
+  let num = stars.toString().substring(0, 1)
+  let array = []
+  for (let i = 1; i <= 5; i++) {
     if (i <= num) {
       array.push(1)
-    }
-    else {
+    } else {
       array.push(0)
     }
   }
@@ -23,6 +22,7 @@ function http(url, callBack) {
     },
     success: function (res) {
       callBack.call(this, res.data)
+      // callBack.apply(this, [res.data])
     },
     fail: function (error) {
       console.log(error)
@@ -31,17 +31,17 @@ function http(url, callBack) {
 }
 
 function convertToCastString(casts) {
-  var castsjoin = ''
-  for (var idx in casts) {
+  let castsjoin = ''
+  for (let idx in casts) {
     castsjoin = castsjoin + casts[idx].name + ' / '
   }
   return castsjoin.substring(0, castsjoin.length - 2)
 }
 
 function convertToCastInfos(casts) {
-  var castsArray = []
-  for (var idx in casts) {
-    var cast = {
+  let castsArray = []
+  for (let idx in casts) {
+    let cast = {
       img: casts[idx].avatars ? casts[idx].avatars.large : '',
       name: casts[idx].name
     }
